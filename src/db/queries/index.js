95df3bc95f4999a5207e1e95ec37730c873bcc59
crno <118383162+Crn0@users.noreply.github.com.>
2024-11-04 14:20:01 +0800

    chore: import queries

--- src/db/queries/index.js
+++ src/db/queries/index.js
@@ -1,5 +1,11 @@
 import get from './get.js';
+import post from './post.js';
+import patch from './patch.js';
+import destroy from './delete.js';
 
 export default {
     get,
+    post,
+    patch,
+    destroy,
 };
