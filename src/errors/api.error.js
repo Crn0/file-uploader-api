0b2becd8776c9cc709ec1f789521e12b95e25349
crno <118383162+Crn0@users.noreply.github.com.>
2024-11-13 17:25:48 +0800

    build(errors/api): rearrange arguments

--- src/errors/api.error.js
+++ src/errors/api.error.js
@@ -2,7 +2,7 @@ import BaseError from './base.error.js';
 
 class APIError extends BaseError {
     constructor(message, httpCode, name = 'API Error', isOperational = true) {
-        super(httpCode, message, name, isOperational);
+        super(message, httpCode, name, isOperational);
     }
 }
 
