b3e7492714318b595bf24866cbb0461a6e1cb3f3
crno <118383162+Crn0@users.noreply.github.com.>
2024-11-24 12:04:54 +0800

    build: enable credentials in cors

--- src/configs/cors/index.js
+++ src/configs/cors/index.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 const corsOptions = {
     origin: process.env.CORS_ORIGINS.split(','),
     methods: process.env.CORS_METHODS.split(','),
+    credentials: true,
     optionsSuccessStatus: 200,
 };
 
